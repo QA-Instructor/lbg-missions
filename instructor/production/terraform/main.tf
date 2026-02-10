a8148569ff7f50c8958ab2afa84da3efd971afd3
QA-Instructor <qainstructorsky@gmail.com>
2026-02-10 17:20:54 +0000

    EDIT: terraform provider versions updated (prod cluster)

--- instructor/production/terraform/main.tf
+++ instructor/production/terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "6.39.0"
+      version = "7.18.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.37.0"
+      version = ">= 3.0.1"
     }
   }
 }
